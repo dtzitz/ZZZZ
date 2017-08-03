017447293440a1a171bd8cef4edf345505110424
Dylan <dtzitz@live.com>
2017-08-03 19:57:22 -0400

    counts the point for the known and unknown edition, still needs logic for time

--- ZZZZZ.py
+++ ZZZZZ.py
@@ -8,10 +8,7 @@ ALPHABET = [0,26,52,78]
 # lol @ paris accord, polluting global like it's my job - REFACTOR PLS
 EDITION_REG = re.compile('[a-zA-Z]')
 DATE_REG = re.compile('[0-9]{8}')
-known_edition = 'BC'
 known_edition_value = []
-known_edition_point = 0
-unknown_edition = 'Z'
 year = 2020
 month = 1
 day = 1
@@ -19,40 +16,53 @@ super_rate = '1 year'
 
 
 def user_input():
-    print("NOTE: This program currently only supports character editions ex. AA AB ect")
+    print("NOTE: This program currently only supports character editions ex. AA AB")
     known_edition = input("Please type the edition with the known effective date: ")
     if EDITION_REG.match(known_edition) != None:
-        pass
+        known_edition_point = count_letters(known_edition)
     else:
+        print('incorrect entry for known edition')
         user_input()
 
-    known_date = input('Please type in the effective date for edition {0} using the format YYYYMMDD: '.format(known_edition))
-    if DATE_REG.match(known_date) != None:
-        year = int(known_date[:4])
-        month = int(known_date[4:6])
-        day = int(known_date[6:])
-        print('Year is {0} Month is {1} Day is {2}'.format(year, month, day))
+    # known_date = input('Please type in the effective date for edition {0} using the format YYYYMMDD: '.format(known_edition))
+    # if DATE_REG.match(known_date) != None:
+    #     year = int(known_date[:4])
+    #     month = int(known_date[4:6])
+    #     day = int(known_date[6:])
+    #     print('Year is {0} Month is {1} Day is {2}'.format(year, month, day))
+    # else:
+    #     user_input()
+
+    unknown_edition = input("Please type the edition with the UNKNOWN EFFECTIVE DATE: ")
+    if EDITION_REG.match(unknown_edition) != None:
+        unknown_edition_point = count_letters(unknown_edition)
     else:
+        print('incorrect entry for unknown edition')
         user_input()
 
-def count_letters():
-    global known_edition_point
-    known_edition_reverse = known_edition[::-1]
-    for i in range(len(known_edition_reverse)):
-        num = LETTERS.find(known_edition_reverse[i])
-        known_edition_point += num + ALPHABET[i] #math is wrong
-        print(known_edition_point)
-    # for letter in known_edition_reverse:
-    #     #add the values to the known_edition_value array then math them later?
-    #     num = LETTERS.find(letter)
-    #     known_edition_value.append(num)
+    print('The known edition point is {0} '.format(known_edition_point))
+    print('The unkown edition point is {0} '.format(unknown_edition_point))
+
+def count_letters(edition):
+    print(edition)
+    edition_reverse = edition[::-1]
+    edition_point = 0
+    for i in range(len(edition_reverse)):
+        if i < 1:
+            edition_point += LETTERS.find(edition_reverse[i])
+        if i == 1:
+            second_letter = LETTERS.find(edition_reverse[i])*26
+            edition_point += second_letter
+        #todo 3 letters?
+            
+    return edition_point
     
 
 
 
 def main():
-    # user_input()
-    count_letters()
+    user_input()
+    # count_letters()
     
 
 if __name__ == '__main__':
