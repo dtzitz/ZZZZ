6eba1dc6b29ff1a88d5c31f5f06fb0bcc2343d02
Dylan <dtzitz@live.com>
2017-08-03 20:31:13 -0400

    almost done

--- ZZZZZ.py
+++ ZZZZZ.py
@@ -1,18 +1,15 @@
 #Python 
 import datetime
+from datetime import timedelta
 import re
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-ALPHABET = [0,26,52,78]
 #tool only works for lettered editions
 # lol @ paris accord, polluting global like it's my job - REFACTOR PLS
 EDITION_REG = re.compile('[a-zA-Z]')
 DATE_REG = re.compile('[0-9]{8}')
-known_edition_value = []
-year = 2020
-month = 1
-day = 1
-super_rate = '1 year'
+# known_edition_value = []
+# super_rate = '1 year'
 
 
 def user_input():
@@ -24,14 +21,22 @@ def user_input():
         print('incorrect entry for known edition')
         user_input()
 
-    # known_date = input('Please type in the effective date for edition {0} using the format YYYYMMDD: '.format(known_edition))
-    # if DATE_REG.match(known_date) != None:
-    #     year = int(known_date[:4])
-    #     month = int(known_date[4:6])
-    #     day = int(known_date[6:])
-    #     print('Year is {0} Month is {1} Day is {2}'.format(year, month, day))
-    # else:
-    #     user_input()
+    known_date = input('Please type in the effective date for edition {0} using the format YYYYMMDD: '.format(known_edition))
+    if DATE_REG.match(known_date) != None:
+        year = int(known_date[:4])
+        month = int(known_date[4:6])
+        day = int(known_date[6:])
+        known_date = datetime.date(year, month, day)
+        print('The known date is {0} '.format(known_date))
+        year = timedelta(days=365)
+        s = known_date - year
+        print('some other date is {0} '.format(s))
+    else:
+        print('you goofed up the date')
+        user_input()
+
+    #todo: get supersession rate
+    super_rate = input('Please type in the supersession rate using num time (1 day, 1 week, 1 month, 6 months, 1 year')
 
     unknown_edition = input("Please type the edition with the UNKNOWN EFFECTIVE DATE: ")
     if EDITION_REG.match(unknown_edition) != None:
@@ -40,8 +45,9 @@ def user_input():
         print('incorrect entry for unknown edition')
         user_input()
 
-    print('The known edition point is {0} '.format(known_edition_point))
-    print('The unkown edition point is {0} '.format(unknown_edition_point))
+    edition_delta = known_edition_point - unknown_edition_point
+
+
 
 def count_letters(edition):
     print(edition)
